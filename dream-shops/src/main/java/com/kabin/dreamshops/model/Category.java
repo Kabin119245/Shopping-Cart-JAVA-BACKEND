ceb08797068ef4ce3b0993234e613e034395d958
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-08 14:02:57 +0545

    relationship mapping

--- dream-shops/src/main/java/com/kabin/dreamshops/model/Category.java
+++ dream-shops/src/main/java/com/kabin/dreamshops/model/Category.java
@@ -1,13 +1,27 @@
 package com.kabin.dreamshops.model;
 
-import java.util.List;
+import ch.qos.logback.core.boolex.EvaluationException;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
     //relationships
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 }
--- dream-shops/src/main/java/com/kabin/dreamshops/model/Image.java
+++ dream-shops/src/main/java/com/kabin/dreamshops/model/Image.java
@@ -1,18 +1,34 @@
 package com.kabin.dreamshops.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Blob;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileName;
     private String fileType;
+    @Lob
     private Blob blob;
     private String downloadUrl;
 
 
     //relationships
 
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
 }
