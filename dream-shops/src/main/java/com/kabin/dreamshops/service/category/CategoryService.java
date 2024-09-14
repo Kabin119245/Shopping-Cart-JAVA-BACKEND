5278ae104cf55cfcf98f2dac073718370337b07b
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-14 20:20:48 +0545

    update category implemented

--- dream-shops/src/main/java/com/kabin/dreamshops/service/category/CategoryService.java
+++ dream-shops/src/main/java/com/kabin/dreamshops/service/category/CategoryService.java
@@ -1,8 +1,55 @@
 package com.kabin.dreamshops.service.category;
 
 
+import com.kabin.dreamshops.exception.ResourceNotFoundException;
+import com.kabin.dreamshops.model.Category;
+import com.kabin.dreamshops.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class CategoryService {
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category Not Found"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long id) {
+       return Optional.ofNullable(getCategoryById(id))
+                .map(oldCategory -> {oldCategory.setName(category.getName());
+                    return categoryRepository.save(oldCategory);
+                }).orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+        categoryRepository.findById(id)
+                .ifPresentOrElse(categoryRepository::delete, () ->  { throw new ResourceNotFoundException("Category Not Found");});
+    }
 }
--- dream-shops/src/main/java/com/kabin/dreamshops/service/category/ICategoryService.java
+++ dream-shops/src/main/java/com/kabin/dreamshops/service/category/ICategoryService.java
@@ -2,7 +2,21 @@ package com.kabin.dreamshops.service.category;
 
 import com.kabin.dreamshops.model.Category;
 
+import java.util.List;
+
 public interface ICategoryService {
 
-    Category getCa
+    Category getCategoryById(Long id);
+    Category addCategory(Category category);
+    Category getCategoryByName(String name);
+
+    List<Category> getAllCategories();
+
+    Category updateCategory(Category category , Long id);
+
+    void deleteCategoryById(Long id);
+
+
+
+
 }
