83943464bc5718ebc3ed0f84ef167abf8df90a10
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-29 23:03:19 +0545

    implementing product controller

--- dream-shops/src/main/java/com/kabin/dreamshops/controlller/ProductController.java
+++ dream-shops/src/main/java/com/kabin/dreamshops/controlller/ProductController.java
@@ -1,4 +1,60 @@
 package com.kabin.dreamshops.controlller;
 
+import com.kabin.dreamshops.model.Product;
+import com.kabin.dreamshops.request.AddProductRequest;
+import com.kabin.dreamshops.response.ApiResponse;
+import com.kabin.dreamshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+
+@RestController
+@RequestMapping("${api.prefix}/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+
+        return ResponseEntity.ok(new ApiResponse("success", products ));
+    }
+
+
+    @GetMapping("/product/{productId}/product}")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long productId) {
+        Product product = productService.getProductById(productId);
+        return ResponseEntity.ok(new ApiResponse("success", product ));
+    }
+
+    //adding product
+    public ResponseEntity<ApiResponse> addProduct(AddProductRequest product) {
+        try {
+            Product theProduct = productService.addProduct(product);
+            return ResponseEntity.ok(new ApiResponse("Add product successfully", theProduct ));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
