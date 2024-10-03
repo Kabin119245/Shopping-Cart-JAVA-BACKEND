aa21b30240be0b968b85c1b6dc4ba007c505fa2b
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-10-03 19:08:31 +0545

    product controller implemented

--- dream-shops/src/main/java/com/kabin/dreamshops/controlller/ProductController.java
+++ dream-shops/src/main/java/com/kabin/dreamshops/controlller/ProductController.java
@@ -1,19 +1,19 @@
 package com.kabin.dreamshops.controlller;
 
+import com.kabin.dreamshops.exception.ResourceNotFoundException;
 import com.kabin.dreamshops.model.Product;
 import com.kabin.dreamshops.request.AddProductRequest;
+import com.kabin.dreamshops.request.ProductUpdateRequest;
 import com.kabin.dreamshops.response.ApiResponse;
 import com.kabin.dreamshops.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 
 @RestController
@@ -38,6 +38,7 @@ public class ProductController {
     }
 
     //adding product
+    @PostMapping("/add")
     public ResponseEntity<ApiResponse> addProduct(AddProductRequest product) {
         try {
             Product theProduct = productService.addProduct(product);
@@ -47,6 +48,118 @@ public class ProductController {
         }
 
     }
+
+
+    @PutMapping("/product/{productId}/update")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody ProductUpdateRequest product, @PathVariable Long productId) {
+        try {
+            Product theProduct = productService.updateProduct(product, productId);
+            return ResponseEntity.ok(new ApiResponse("Updated product successfully", theProduct ));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+
+    //to delete the product
+
+    @DeleteMapping("/product/{productId}/delete")
+    public ResponseEntity<ApiResponse> deleteProduct(@PathVariable Long productId) {
+
+        try {
+            productService.deleteProductById(productId);
+            return ResponseEntity.ok(new ApiResponse("Product Deleted Successfully", productId));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+    //get product by brand and name
+    @GetMapping("/products/by/brand-and-name")
+    public ResponseEntity<ApiResponse> getProductByBrandAndName(@RequestParam String brandName, @RequestParam String productName) {
+        try {
+            List<Product> products = productService.getProductsByBrandAndName(brandName, productName);
+
+            if(products.isEmpty()) {
+                return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Product not found with name" , null));
+            }
+            return ResponseEntity.ok(new ApiResponse("success", products ));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+    //get product by category and brand
+    @GetMapping("/products/by/category-and-brand")
+    public ResponseEntity<ApiResponse> getProductByCategoryAndBrand(@RequestParam String category, @RequestParam String brand) {
+        try {
+            List<Product> products = productService.getProductsByCategoryAndBrand(category, brand);
+            if(products.isEmpty()) {
+                return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Product not found" , null));
+            }
+            return ResponseEntity.ok(new ApiResponse("success", products ));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+    //get products by name
+    @GetMapping("/products/{name}/products")
+    public ResponseEntity<ApiResponse> getProductByName(@PathVariable String name) {
+        try {
+            List<Product> products = productService.getProductsByName(name);
+            if(products.isEmpty()) {
+                return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Product not found" , null));
+            }
+            return ResponseEntity.ok(new ApiResponse("success", products ));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    //get products by brand
+    @GetMapping("/products/by/brand")
+    public ResponseEntity<ApiResponse> getProductByBrand(@RequestParam String brand) {
+        try {
+            List<Product> products = productService.getProductsByBrand(brand);
+            if(products.isEmpty()) {
+                return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Product not found" , null));
+            }
+            return ResponseEntity.ok(new ApiResponse("success", products ));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    //get products by category
+    @GetMapping("/products/{category}/all/products")
+    public ResponseEntity<ApiResponse> getProductByCategory(@PathVariable String category) {
+        try {
+            List<Product> products = productService.getProductsByCategory(category);
+            if(products.isEmpty()) {
+                return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Product not found" , null));
+            }
+            return ResponseEntity.ok(new ApiResponse("success", products ));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
