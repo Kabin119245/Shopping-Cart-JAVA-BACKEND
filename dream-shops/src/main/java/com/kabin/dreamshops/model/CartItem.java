8de69ca6d6bfd996640272a2603621b203023ba3
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-10-04 23:35:40 +0545

    cart and cartItems models added

---
+++ dream-shops/src/main/java/com/kabin/dreamshops/model/CartItem.java
@@ -0,0 +1,42 @@
+package com.kabin.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal uniPrice;
+    private BigDecimal totalPrice;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public void setTotalPrice() {
+        this.totalPrice = this.uniPrice.multiply(new BigDecimal(quantity));
+    }
+
+
+
+
+}
